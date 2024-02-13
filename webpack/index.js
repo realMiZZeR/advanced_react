75ab1070672958bd150d7a7d825d9ef4fcca5ce0
nikolaev.mv <maxcan2work@gmail.com>
2024-02-13 17:02:46 +0700

    написал конфигурацию для webpack из записи лекции, но немного изменив

---
+++ webpack/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM  from 'react-dom';
+
+const root = document.getElementById("root");
+const Hello = () => <h1>Hello!</h1>
+
+ReactDOM.render(<Hello />, root)
